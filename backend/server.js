ddbbf49d8337d5c8e8ef4ace4dcd9a4a84446159
Anuar <a.bayakhmetov@kdss.tech>
2024-12-09 02:00:29 +0500

    feat: added changes on port

--- backend/server.js
+++ backend/server.js
@@ -198,7 +198,7 @@ app.post('/api/waste-level', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
